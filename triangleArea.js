67d5663dc5fdc9f4bf049d39a0298b79b9fca26d
Arkadiusz Cichosz <cichorek87220@gmail.com>
2017-02-02 17:01:30 +0100

    Update triangleArea.js
    fixed function

--- triangleArea.js
+++ triangleArea.js
@@ -1,15 +1,18 @@
 //exercise 8.6
-var base, height, triangle1Area, triangle2Area, triangle3Area;
+var base, height, triangle1Area, triangle2Area, triangle3Area, warning;
 function getTriangleArea (base, height) {
-	if(base>0 && height>0) {
-		return base*height/2;
+	if(base > 0 && height > 0) {
+		return base * height / 2;
 	}
 	else {
-		console.log('wysokość trójkąta bądź długość podstawy mają wartość ujemną bądź zero, operacja zakończona błędem');
+		return warning = 'wysokość trójkąta bądź długość podstawy mają wartość ujemną lub wynoszącą zero, operacja zakończona błędem';
 	}
 }
 
 console.log(getTriangleArea(10,6));
-triangle1Area=getTriangleArea(10,15);
-triangle2Area=getTriangleArea(3,6);
-triangle3Area=getTriangleArea(2,5);
+triangle1Area = getTriangleArea(10,-15);
+triangle2Area = getTriangleArea(3,6);
+triangle3Area = getTriangleArea(2,5);
+console.log('traiangle1Area = ' + triangle1Area);
+console.log('traiangle2Area = ' + triangle2Area);
+console.log('traiangle3Area = ' + triangle3Area);
